7f2822fb576f8bfcd511545e73c6d7e314d26a73
Aamir Fatahayab <aamir.fatahyab@gmail.com>
2019-10-14 12:16:27 +0530

    Add project files.

---
+++ CoreWebAppDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreWebAppDemo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
